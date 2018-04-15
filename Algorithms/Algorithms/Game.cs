c92d6d66b38004167d707ea8aa445d5179e347a1
j0035 <karsten.helmut@gmail.com>
2018-04-15 09:51:24 +0200

    First push

---
+++ Algorithms/Algorithms/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Game
+    {
+        Move Snek = new Move();
+        ConsoleKeyInfo cki;
+
+        public void runSnek()
+        {
+
+            while (true)
+            {
+                // Check whether the user did some input
+                if (Console.KeyAvailable)
+                {
+                    // User input - react to the users input
+
+                    cki = Console.ReadKey();
+
+                    int passer = passDirectionChange(cki);
+                    if (passer != 100)
+                    {
+                        Snek.validateDirection(passer);
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+
+                }
+                else
+                {
+                    // No user input - the gaming loop does a time step
+
+                    // Move according to the currrent user symbol speed
+                    Snek.changeCoordinates();
+                    Console.Write(Snek.getCoordinate()[0] + ", ");
+                    Console.WriteLine(Snek.getCoordinate()[1]);
+                    // Wait some time
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+
+        private int passDirectionChange(ConsoleKeyInfo pry)
+        {
+            if (pry.Key == ConsoleKey.UpArrow)
+            {
+                return 0;
+            }
+
+            if (pry.Key == ConsoleKey.DownArrow)
+            {
+                return 1;
+            }
+
+            if (pry.Key == ConsoleKey.LeftArrow)
+            {
+                return 2;
+            }
+
+            if (pry.Key == ConsoleKey.RightArrow)
+            {
+                return 3;
+            }
+
+            if (pry.Key == ConsoleKey.X)
+            {
+                return 100;
+            }
+
+            return 100;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Move.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Move
+    {
+
+        enum direction
+        {
+            up,
+            down,
+            left,
+            right
+        }
+
+        direction directionOfSnek;
+
+        int[] coordinate = { 0, 0 };
+
+        public Move()
+        {
+            directionOfSnek = direction.right;
+            coordinate[0] = 40;
+            coordinate[1] = 40;
+        }
+
+        public void changeCoordinates()
+        {
+            switch (directionOfSnek)
+            {
+                case direction.up:
+                    {
+                        coordinate[1] = coordinate[1] + 1;
+                        break;
+                    }
+                case direction.down:
+                    {
+                        coordinate[1] = coordinate[1] - 1;
+                        break;
+                    }
+                case direction.right:
+                    {
+                        coordinate[0] = coordinate[0] + 1;
+                        break;
+                    }
+                case direction.left:
+                    {
+                        coordinate[0] = coordinate[0] - 1;
+                        break;
+                    }
+            }
+        }
+
+        public int[] getCoordinate()
+        {
+            return coordinate;
+        }
+
+
+        public void validateDirection(int newDirection)
+        {
+            if (valUpDownDirection(directionOfSnek) && valLeftRightDirection((direction)newDirection) || valLeftRightDirection(directionOfSnek) && valUpDownDirection((direction)newDirection))
+            {
+                changeDirection(newDirection);
+            }
+        }
+
+        private void changeDirection(int newDirection)
+        {
+            if (newDirection >= 0 && newDirection <= 3)
+            {
+                directionOfSnek = (direction)newDirection;
+            }
+        }
+
+        //validators
+        private Boolean valUpDownDirection(direction Dir)
+        {
+            if (Dir == direction.up || Dir == direction.down)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private Boolean valLeftRightDirection(direction Dir)
+        {
+            if (Dir == direction.left || Dir == direction.right)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
